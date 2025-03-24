b8e2bbc0e14ba7c9cf8fec7c28116b8505e8303e
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2025-03-24 21:17:26 +0530

    add errors

---
+++ server/src/helpers/constants.py
@@ -0,0 +1,12 @@
+__all__ = ["BadRequestException", "CustomException", "DuplicateValueException", "ForbiddenException", "NotFoundException", "RateLimitException", "UnauthorizedException", "UnprocessableEntityException"]
+
+from fastcrud.exceptions.http_exceptions import (
+    BadRequestException,
+    CustomException,
+    DuplicateValueException,
+    ForbiddenException,
+    NotFoundException,
+    RateLimitException,
+    UnauthorizedException,
+    UnprocessableEntityException,
+)
