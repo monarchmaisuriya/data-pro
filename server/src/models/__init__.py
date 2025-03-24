b00207349988898702053d713ce92ba30a4b4676
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2025-03-24 21:10:08 +0530

    update imports, exports

--- server/src/models/__init__.py
+++ server/src/models/__init__.py
@@ -1,3 +1,12 @@
-__all__ = ["Task","TaskPriority","TaskStatus"]
+from sqlmodel import SQLModel
 
-from src.models.tasks import Task, TaskPriority, TaskStatus
+NAMING_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+metadata = SQLModel.metadata
+metadata.naming_convention = NAMING_CONVENTION
--- server/src/models/tasks.py
+++ server/src/models/tasks.py
@@ -2,7 +2,10 @@ from datetime import datetime
 from enum import Enum, IntEnum
 from uuid import UUID, uuid4
 
-from sqlmodel import Field, SQLModel
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlmodel import Field
+
+from src.models import SQLModel
 
 
 class TaskPriority(IntEnum):
@@ -23,6 +26,9 @@ class Task(SQLModel, table=True):
     title: str
     description: str|None = Field(default=None, nullable=True)
     priority: TaskPriority = Field(default=TaskPriority.MEDIUM)
+    status: TaskStatus = Field(default=TaskStatus.PENDING)
+    parameters: dict = Field(default_factory=dict, sa_type=JSONB)
+    result: dict = Field(default_factory=dict, sa_type=JSONB)
     due_date: datetime | None = Field(default=None, nullable=True)
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime = Field(
@@ -31,6 +37,39 @@ class Task(SQLModel, table=True):
     completed_at: datetime | None = Field(default=None, nullable=True)
     deleted: bool = Field(default=False, nullable=True)
     deleted_at: datetime | None = Field(default=None, nullable=True)
-    status: TaskStatus = Field(default=TaskStatus.PENDING)
 
+class TaskCreate(SQLModel):
+    title: str
+    description: str | None
+    priority: TaskPriority
+    parameters: dict
+    due_date: datetime | None
+
+class TaskRead(SQLModel):
+    id: UUID
+    title: str
+    description: str | None
+    priority: TaskPriority
+    status: TaskStatus
+    parameters: dict
+    result: dict
+    due_date: datetime | None
+    created_at: datetime
+    updated_at: datetime
+    completed_at: datetime | None
+    deleted: bool
+    deleted_at: datetime | None
+
+class TaskUpdate(SQLModel):
+    title: str | None
+    description: str | None
+    priority: TaskPriority | None
+    status: TaskStatus | None
+    parameters: dict | None
+    result: dict | None
+    due_date: datetime | None
+
+class TaskDelete(SQLModel):
+    deleted: bool
+    deleted_at: datetime
 
